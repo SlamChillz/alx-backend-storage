30185f26b3bb9feafe01e809e483e61b56ad20f8
slamchillz <mendyslam@gmail.com>
2023-01-12 23:32:23 +0100

    avoid using column names as variables

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -7,20 +7,20 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
 	DECLARE i INT;
 	DECLARE n INT;
-	DECLARE Id INT;
+	DECLARE d INT;
 	SELECT COUNT(*) FROM users INTO n;
 	SET i = 0;
 	WHILE i < n DO
-		SELECT id INTO Id FROM users LIMIT i, 1;
+		SELECT id INTO d FROM users LIMIT i, 1;
 		UPDATE users SET average_score = (
 			SELECT SUM(x.score * x.weight) / SUM(x.weight)
 			FROM (
 				SELECT c.score, p.weight 
 				FROM corrections c
 				LEFT JOIN projects p ON c.project_id = p.id
-				WHERE c.user_id = Id
+				WHERE c.user_id = d
 			) x
-		) WHERE id = Id;
+		) WHERE id = d;
 		SET i = i + 1;
 	END WHILE;
 END;
