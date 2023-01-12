1350e9e1a623c07d1e2108760928d760c153f7bd
slamchillz <mendyslam@gmail.com>
2023-01-12 23:23:03 +0100

    tell alx we in 2023 already!

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT
   band_name,
-  IFNULL (split, YEAR (CURDATE())) - formed As lifespan
+  IFNULL (split, 2022) - formed As lifespan
 FROM
   metal_bands
 WHERE
